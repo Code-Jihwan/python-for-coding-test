94ad2f04b0a3196855c6df00c886340c2c9a93d9
jihwan <jihwan3194@naver.com>
2022-08-03 23:28:33 +0900

    json 파일, 작성 소스코드

--- Complexity/jsontest2.py
+++ Complexity/jsontest2.py
@@ -1,6 +1,6 @@
 import json
 
-file_path = '/Users/joojihwan/Desktop/python_for_coding_test/Complexity/jsontest.json'
+file_path = '/Users/joojihwan/Desktop/python_for_coding_test/Complexity/jsonfile.json'
 
 with open(file_path, 'r') as fp:    #읽기모드 접근
   data = json.load(fp)    #파일객체(fp)   #data에 jsontest.json 에서 읽어온 데이터들이 딕션너리 형태로 저장
