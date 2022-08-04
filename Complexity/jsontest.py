684e034657691e0675c0d05303eb1b64fb551505
jihwan <jihwan3194@naver.com>
2022-08-04 19:02:15 +0900

    주석 수정

--- Complexity/jsontest.py
+++ Complexity/jsontest.py
@@ -9,7 +9,7 @@ a['test'] = True
 #저장할 파일 경로(이름)
 file_path = '/Users/joojihwan/Desktop/python_for_coding_test/Complexity/jsontest.json'
 
-with open(file_path, 'w', encoding='UTF-8') as fp:
-    json.dump(a, fp, indent=4)    #파이썬 오브젝트(a), 파일객체(fp)
+with open(file_path, 'w', encoding='UTF-8') as fp:      #쓰기 모드 접근
+    json.dump(a, fp, indent=4)    #파이썬 오브젝트(a), 파일객체(fp), indent 옵션으로 들여쓰기 가능
  
 #json 테스트 코드.
